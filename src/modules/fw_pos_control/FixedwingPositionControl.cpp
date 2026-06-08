38b8f5a10a95173fab98aa4ad623b86d2138139f
Vladimir Svetlakov <vova.svetl@mail.ru>
2026-06-08 08:16:33 +0300

    feat: update scripts

--- src/modules/fw_pos_control/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control/FixedwingPositionControl.cpp
@@ -929,7 +929,7 @@ FixedwingPositionControl::control_auto(const float control_interval, const Vecto
 	}
 
 	/* Copy thrust and pitch values from tecs */
-	_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+	_att_sp.pitch_body = get_tecs_pitch();
 
 	if (!_vehicle_status.in_transition_to_fw) {
 		publishLocalPositionSetpoint(current_sp);
@@ -961,7 +961,7 @@ FixedwingPositionControl::control_auto_fixed_bank_alt_hold(const float control_i
 		_att_sp.thrust_body[0] = min(get_tecs_thrust(), _param_fw_thr_max.get());
 	}
 
-	_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+	_att_sp.pitch_body = get_tecs_pitch();
 }
 
 void
@@ -987,7 +987,7 @@ FixedwingPositionControl::control_auto_descend(const float control_interval)
 	_att_sp.yaw_body = 0.f;
 
 	_att_sp.thrust_body[0] = (_landed) ? _param_fw_thr_min.get() : min(get_tecs_thrust(), _param_fw_thr_max.get());
-	_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+	_att_sp.pitch_body = get_tecs_pitch();
 }
 
 uint8_t
@@ -1492,7 +1492,7 @@ FixedwingPositionControl::control_auto_takeoff(const hrt_abstime &now, const flo
 				_att_sp.thrust_body[0] = (_landed) ? min(_param_fw_thr_idle.get(), 1.f) : get_tecs_thrust();
 			}
 
-			_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+			_att_sp.pitch_body = get_tecs_pitch();
 			_att_sp.yaw_body = _yaw; // yaw is not controlled, so set setpoint to current yaw
 
 		} else {
@@ -1665,7 +1665,7 @@ FixedwingPositionControl::control_auto_landing_straight(const hrt_abstime &now, 
 		/* set the attitude and throttle commands */
 
 		// TECS has authority (though constrained) over pitch during flare, throttle is hard set to idle
-		_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+		_att_sp.pitch_body = get_tecs_pitch();
 
 		const float roll_factor = 1.0f / math::max(cosf(_att_sp.roll_body), 0.5f);
 		_att_sp.pitch_body = math::constrain(_att_sp.pitch_body * roll_factor * 1.4f,
@@ -1722,7 +1722,7 @@ FixedwingPositionControl::control_auto_landing_straight(const hrt_abstime &now, 
 
 		/* set the attitude and throttle commands */
 
-		_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+		_att_sp.pitch_body = get_tecs_pitch();
 
 		const float roll_factor = 1.0f / math::max(cosf(_att_sp.roll_body), 0.5f);
 		_att_sp.pitch_body = math::constrain(_att_sp.pitch_body * roll_factor * 1.4f,
@@ -1878,7 +1878,7 @@ FixedwingPositionControl::control_auto_landing_circular(const hrt_abstime &now, 
 		/* set the attitude and throttle commands */
 
 		// TECS has authority (though constrained) over pitch during flare, throttle is hard set to idle
-		_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+		_att_sp.pitch_body = get_tecs_pitch();
 
 		const float roll_factor = 1.0f / math::max(cosf(_att_sp.roll_body), 0.5f);
 		_att_sp.pitch_body = math::constrain(_att_sp.pitch_body * roll_factor * 1.4f,
@@ -1937,7 +1937,7 @@ FixedwingPositionControl::control_auto_landing_circular(const hrt_abstime &now, 
 
 		/* set the attitude and throttle commands */
 
-		_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+		_att_sp.pitch_body = get_tecs_pitch();
 
 		const float roll_factor = 1.0f / math::max(cosf(_att_sp.roll_body), 0.5f);
 		_att_sp.pitch_body = math::constrain(_att_sp.pitch_body * roll_factor * 1.4f,
@@ -2006,7 +2006,7 @@ FixedwingPositionControl::control_manual_altitude(const float control_interval, 
 	_att_sp.yaw_body = _yaw; // yaw is not controlled, so set setpoint to current yaw
 
 	_att_sp.thrust_body[0] = min(get_tecs_thrust(), throttle_max);
-	_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+	_att_sp.pitch_body = get_tecs_pitch();
 
 	const float roll_factor = 1.0f / math::max(cosf(_att_sp.roll_body), 0.5f);
 	_att_sp.pitch_body = math::constrain(_att_sp.pitch_body * roll_factor * 1.4f,
@@ -2111,7 +2111,7 @@ FixedwingPositionControl::control_manual_position(const float control_interval, 
 	}
 
 	_att_sp.thrust_body[0] = min(get_tecs_thrust(), throttle_max);
-	_att_sp.pitch_body = get_tecs_pitch() + math::radians(12.0f);
+	_att_sp.pitch_body = get_tecs_pitch();
 
 	const float roll_factor = 1.0f / math::max(cosf(_att_sp.roll_body), 0.5f);
 	_att_sp.pitch_body = math::constrain(_att_sp.pitch_body * roll_factor * 1.4f,
@@ -2628,6 +2628,8 @@ FixedwingPositionControl::tecs_update_pitch_throttle(const float control_interva
 	if (fabsf(_alt_error) < 6.0f) {
 		alt_sp = _current_altitude;
 	}
+
+	calibrated_airspeed_sp *= 0.72f;
 
 	_tecs.update(_pitch - radians(_param_fw_psp_off.get()),
 		     _current_altitude,
